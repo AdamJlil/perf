3226830131d0fb3546bbf6890ce4a4a92ed1b53b
Adam Jlil <ajlil@duboisag.com>
2024-10-13 20:08:45 -0400

    Dark / Light mode, Nutrition Plans added

---
+++ components/Sections/Nutrition/BlocFour.vue
@@ -0,0 +1,40 @@
+<!-- components/Sections/Nutrition/Header.vue -->
+<template>
+  <header
+    class="bg-white dark:bg-black flex flex-col md:flex-row items-center w-full py-20"
+    style="font-family: Montserrat"
+  >
+    <!-- Center Section: Content -->
+    <div class="w-full flex flex-col content-around p-8 bg-white dark:bg-black">
+      <!-- Title -->
+      <h1
+        class="text-2xl lg:text-4xl md:text-2xl leading-loose font-normal text-center mb-6 py-10 uppercase text-black dark:text-white"
+      >
+        What are you <br />
+        waiting for?
+      </h1>
+
+      <!-- Buttons -->
+      <div
+        class="flex flex-col md:flex-row gap-4 md:gap-8 w-full md:max-w-none justify-center items-center z-10 py-10 md:py-5"
+      >
+        <NuxtLink
+          to="/login"
+          class="text-black dark:text-white border border-black dark:border-white py-2 px-6 md:px-10 text-center text-sm md:text-base lg:text-lg sm:text-sm max-md:w-[140px] md:w-[200px] lg:w-[300px]"
+          style="font-weight: 300"
+          aria-label="Log In"
+        >
+          LOG IN
+        </NuxtLink>
+        <NuxtLink
+          to="/join"
+          class="text-black dark:text-white border border-black dark:border-white py-2 px-6 md:px-10 text-center text-sm md:text-base lg:text-lg max-md:w-[140px] md:w-[200px] lg:w-[300px]"
+          style="font-weight: 300"
+          aria-label="Join In"
+        >
+          JOIN
+        </NuxtLink>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ pages/nutritionPlans.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import Header from "~/components/Sections/Nutrition/Header.vue";
+import Bloc1 from "~/components/Sections/Nutrition/BlocOne.vue";
+import Bloc2 from "~/components/Sections/Nutrition/BlocTwo.vue";
+import Bloc3 from "~/components/Sections/Nutrition/BlocThree.vue";
+import Bloc4 from "~/components/Sections/Nutrition/BlocFour.vue";
+</script>
+
+<template>
+  <Header />
+  <Bloc1 />
+  <Bloc2 />
+  <Bloc3 />
+  <Bloc4 />
+</template>
